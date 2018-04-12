b454d23e296ae3224dbf87a186db5ed3fee9ee75
HarryStevens <harryjosephstevens@gmail.com>
2018-04-13 00:11:53 +0530

    Tabs

--- src/draw/draw.js
+++ src/draw/draw.js
@@ -2,8 +2,8 @@ export default function draw(){
 
   // check for geospatial data
   if (this.meta.geo.length == 0) {
-  	console.error("You must pass TopoJSON data through swiftmap.geometry() before you can draw the map.")
-  	return;
+    console.error("You must pass TopoJSON data through swiftmap.geometry() before you can draw the map.")
+    return;
   }
 
   // basic drawing
