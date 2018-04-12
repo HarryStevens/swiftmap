0c6ee3123be28b3bfdb35a49f1e675838f75f9c1
HarryStevens <harryjosephstevens@gmail.com>
2018-04-12 23:53:23 +0530

    Type checking for colorOther()

--- src/scheme/colorOther.js
+++ src/scheme/colorOther.js
@@ -1,7 +1,12 @@
 export default function colorOther(color){
   if (!color) return this.meta.colorOther;
 
-  this.meta.colorOther = color;
+  // type checking
+  if (typeof color !== "string"){
+  	console.warn("The argument passed to scheme.colorOther() must be a string. The color will not be updated, and defaults to '#ccc'.");
+  } else {
+  	this.meta.colorOther = color;
+  }
 
   return this;
 }
