4ff2b2898c8f848c7df228e449d709f5ec5ee298
HarryStevens <harryjosephstevens@gmail.com>
2018-04-23 10:49:28 -0400

    Add new files

---
+++ src/scheme/breaks.js
@@ -0,0 +1,37 @@
+export default function breaks(breakargument){
+  if (!breakargument) return this.meta.breaklist;
+
+  // scope the scheme
+  var scheme = this;
+
+  // breaktype must match one of the available modes
+  var available_modes = ["e", "q", "l", "k"]
+
+  // If the argument is not of a valid type, warn and default to quantile breaks
+  if (typeof breakargument !== "string" && !Array.isArray(breakargument)) {
+    console.warn("The argument passed to scheme.breaks() must be either a string or an array. The scheme will default to using quantile breaks.");
+    this.meta.breaklist = limits(tab.map(scheme.meta.values), scheme.meta.breaktype, scheme.meta.colors.length);
+  }
+
+  // If the argument is a string, compute the breaklist based on the breaktype
+  else if (typeof breakargument === "string") {
+
+    // If the breaktype passed does not match an available mode, set it to the default.
+    if (available_modes.indexOf(breakargument) == -1) {
+      console.warn("You must specify the scheme's breaktype as either 'e', 'q', 'l', or 'k'. The breaktype will default to 'q'.");
+    }
+
+    // Otherwise, update the breaktype
+    else {
+      this.meta.breaktype = breakargument;  
+    }
+  } 
+
+  // If the argument is an array, we're using custom breaks, so just set the breaklist.
+  else {
+    this.meta.breaktype = "c";
+    this.meta.breaklist = breakargument;
+  }
+  
+  return this;
+}
