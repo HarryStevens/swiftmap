f92ac64f32703f6bbc1d3766852cb91d9f6013e3
HarryStevens <harryjosephstevens@gmail.com>
2018-04-27 08:18:03 -0400

    Clean code

--- src/map/drawLabels.js
+++ src/map/drawLabels.js
@@ -18,7 +18,7 @@ export default function drawLabels(key, layer) {
 
   // Check the type of the optional layer parameter.
   if (layer && !isString(layer) && !isNumber(layer)) {
-    console.warn("You must specify the layer as a string or a number. Layer will default to " + swiftmap.meta.last_layer);
+    console.warn("You must specify the layer as a string or a number. Layer will default to " + swiftmap.meta.last_layer + ".");
     layer = this.meta.last_layer;
   }
 
