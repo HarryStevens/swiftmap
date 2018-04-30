7321ca3c8a3922ca52d89e48137cfb0d1cef6af6
HarryStevens <harryjosephstevens@gmail.com>
2018-04-30 15:25:28 -0400

    Fix tabs

--- src/map/pointsUtility.js
+++ src/map/pointsUtility.js
@@ -2,29 +2,30 @@ import polylabel from "../../lib/polylabel";
 
 // Determines the coordinates of text or points, depending on if the layer is polygons or points.
 export default function pointsUtility(swiftmap, d){
-	// In geometries of type MultiPolygon,
-	// get the polylabel that is closest to centroid.
-	var closest;
+  // In geometries of type MultiPolygon,
+  // get the polylabel that is closest to centroid.
 
-	if (d.geometry.type == "MultiPolygon"){	
-		var min = Infinity,
+  // TODO: Use a while loop for multi-multi polgyons.
+  var closest;
+
+  if (d.geometry.type == "MultiPolygon"){  
+    var min = Infinity,
         c = swiftmap.path.centroid(d);
 
-		for (var i = 0, l = d.geometry.coordinates.length; i < l; i++){
+    for (var i = 0, l = d.geometry.coordinates.length; i < l; i++){
 
-			var p = swiftmap.meta.projection.function(polylabel(d.geometry.coordinates[i], .01)),
-					dist = Math.sqrt(Math.pow(p[0] - c[0], 2) + Math.pow(p[1] - c[1], 2));
+      var p = swiftmap.meta.projection.function(polylabel(d.geometry.coordinates[i], .01)),
+          dist = Math.sqrt(Math.pow(p[0] - c[0], 2) + Math.pow(p[1] - c[1], 2));
 
-			if (Number.isFinite(dist) && dist < min){
-				min = dist;
-				closest = p;
-			}
-		}
-	}
+      if (Number.isFinite(dist) && dist < min){
+        min = dist;
+        closest = p;
+      }
+    }
+  }
 
-	// TODO: Find a good labeling algorithm.
   return d.properties.swiftmap.layer == "polygons" ?
-	  d.geometry.type === "MultiPolygon" ? closest || swiftmap.path.centroid(d) :
-	  swiftmap.meta.projection.function(polylabel(d.geometry.coordinates, .01)) :
-	  swiftmap.meta.projection.function(d.geometry.coordinates);
+    d.geometry.type === "MultiPolygon" ? closest || swiftmap.path.centroid(d) :
+    swiftmap.meta.projection.function(polylabel(d.geometry.coordinates, .01)) :
+    swiftmap.meta.projection.function(d.geometry.coordinates);
 }
